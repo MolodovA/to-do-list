b5567ac00e918b2f8ec70c879c4cb210b7b3ce12
MolodovA <80354289+MolodovA@users.noreply.github.com>
2022-05-28 18:09:40 +0300

    Updates

---
+++ precache-manifest.bdd49b104476a1777d3b206bc82b2515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec9e9f43aa38dfcb506d456d85b1b6a",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "7e7d51a679199360687d",
+    "url": "/to-do-list/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "ed9679c58f61824bd057",
+    "url": "/to-do-list/static/js/2.8e121f72.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/to-do-list/static/js/2.8e121f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7d51a679199360687d",
+    "url": "/to-do-list/static/js/main.b3d42015.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
